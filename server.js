6b3d19306d2c921176c491d49909e9d739bbc071
Vatsal Lath <lathvatsal@gamil.com>
2026-03-17 17:36:16 +0530

    test ci cd working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 App is running on EC2!");
+  res.send("🔥 CI/CD WORKING PERFECTLY 🚀");
 });
 
 // VERY IMPORTANT LINE
